24a0dc75e86fc5d404cc2b9d0120fecdbb48e770
dani-xhack <131921895+dani-xhack@users.noreply.github.com>
2023-05-24 13:26:15 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
+// Get the sidebar element
+var sidebar = document.getElementById("mySidebar");
 
+// Add event listeners for mouseenter and mouseleave events
+sidebar.addEventListener("mouseenter", showSidebar);
+sidebar.addEventListener("mouseleave", hideSidebar);
+
+function showSidebar() {
+  // Adjust the left CSS property to expand the sidebar
+  sidebar.style.left = "0";
+}
+
+function hideSidebar() {
+  // Adjust the left CSS property to collapse the sidebar
+  sidebar.style.left = "-230px";
+}
